e57a350151f9e6e0f13e76fcb5b71806fa66cec7
ssanthoshss <santhosh@skcript.com>
2021-04-29 20:16:40 +0530

    Changed root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  
+  root to: 'posts#index'
+
+  resources :posts, except: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
