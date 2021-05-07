1ebe783b64d1edb6e72f564f9b18cb5142b49bbd
ssanthoshss <santhosh@skcript.com>
2021-05-07 18:42:13 +0530

    Update type of action firebase

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::PostsController < ApplicationController
 
 	def webhook
 		firebase_db = FirebaseDatabaseService.new
-		firebase_db.update('posts', {id: params["queryResult"]["parameters"]["number"].to_i})
+		firebase_db.update('posts', {id: params["queryResult"]["parameters"]["number"].to_i, action: params["queryResult"]["action"].to_s})
 		render json: { success: true }
 	end
 end
